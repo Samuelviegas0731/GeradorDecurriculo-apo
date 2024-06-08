15203b9ada07af533f4268896a305ae3689cff5a
Samuel <samuel310705@edu.unipar.br>
2024-06-08 17:16:58 -0300

    bootstrap

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
     <!-- Define o conjunto de caracteres do documento como UTF-8 -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Configura a viewport para permitir um design responsivo -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- utiliza o metodo bootstrap -->
     <title>Formulário de Dados Pessoais</title>
     <!-- Define o título da página -->
     <link rel="stylesheet" href="css/style.css">
